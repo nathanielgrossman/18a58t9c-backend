213598f0fcc32e874f57238568dc55789c0e0966
nathanielgrossman <nategrossman@mac.com>
2019-01-18 14:47:28 -0800

    reworked get-all function to take a query string and optionally return only specific chunk of images

--- get-all.js
+++ get-all.js
@@ -3,29 +3,41 @@ const mongoose = require('mongoose');
 const Image = require('./image-schema');
 
 exports.handler = (event, context, callback) => {
+    let start = null;
+    let size = null;
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
         .then(() => {
-            Image.find({}, (err, images) => {
-                if (err) {
-                    let response = {
-                        "statusCode": 500,
-                        "body": JSON.stringify("error"),
-                        "isBase64Encoded": false
-                    };
-                    mongoose.disconnect();
-                    callback(null, response);
-                } else {
-                    let response = {
-                        "statusCode": 200,
-                        "headers": {
-                          "Access-Control-Allow-Origin" : "*"
-                        },
-                        "body": JSON.stringify(images),
-                        "isBase64Encoded": false
-                    };
-                    mongoose.disconnect();
-                    callback(null, response);
-                }
+            if (event.queryStringParameters) {
+                start = parseInt(event.queryStringParameters.start);
+                size = parseInt(event.queryStringParameters.size);
+            }
+            Image.count().exec((err, count) => {
+                Image.find().sort({ $natural: -1 }).skip(start).limit(size).exec({}, (err, images) => {
+                    if (err) {
+                        let response = {
+                            "statusCode": 500,
+                            "body": JSON.stringify(err),
+                            "isBase64Encoded": false
+                        };
+                        mongoose.disconnect();
+                        callback(null, response);
+                    } else {
+                        let response = {
+                            "statusCode": 200,
+                            "headers": {
+                                "Access-Control-Allow-Origin": "*",
+                                "Access-Control-Allow-Credentials": true
+                            },
+                            "body": JSON.stringify({
+                                total: count,
+                                images: images
+                            }),
+                            "isBase64Encoded": false
+                        };
+                        mongoose.disconnect();
+                        callback(null, response);
+                    }
+                })
             })
         })
 }
