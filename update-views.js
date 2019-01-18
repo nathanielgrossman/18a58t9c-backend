dff0946cae3ba7e2799b5e9534fb59022ba5d4d2
nathanielgrossman <nategrossman@mac.com>
2019-01-18 14:48:15 -0800

    added lambda to update views on an image entry in db

---
+++ update-views.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Image = require('./image-schema');
+
+exports.handler = (event, context, callback) => {
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+        .then(() => {
+            Image.findByIdAndUpdate(
+                event.id, { $inc: { views: 1 } },
+                (err, image) => {
+                    if (err) {
+                        let response = {
+                            "statusCode": 500,
+                            "body": JSON.stringify("error"),
+                            "isBase64Encoded": false
+                        };
+                        mongoose.disconnect();
+                        callback(null, response);
+                    } else {
+                        let response = {
+                            "statusCode": 200,
+                            "body": JSON.stringify(event.id + ' updated'),
+                            "isBase64Encoded": false
+                        };
+                        mongoose.disconnect();
+                        callback(null, response);
+                    }
+                })
+        })
+}
