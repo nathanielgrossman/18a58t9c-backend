fc5acb120b3a785d6574c40af8e767e77a799cc9
nathanielgrossman <nategrossman@mac.com>
2019-01-18 14:49:16 -0800

    upload function now creates thumbnails as well

--- upload.js
+++ upload.js
@@ -3,6 +3,7 @@ const stream = require('stream');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 const Image = require('./image-schema');
@@ -20,13 +21,21 @@ const jpegOptions = {
     quality: 90
 }
 
+const thumbOptions = {
+    quality: 80
+}
+
+const thumbSize = 600;
+
 exports.handler = (event, context, callback) => {
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
         .then(() => {
-            let oneUploaded = false;
+            let uploaded = 0;
 
+            let rgb = `rgb(${event.color.join(', ')})`;
             const newImage = new Image({
-                original: event.name
+                original: event.name,
+                color: rgb
             });
             newImage.save((err, image) => {
                 if (err) console.log(err);
@@ -38,19 +47,22 @@ exports.handler = (event, context, callback) => {
 
                 const sharpWebp = sharp(decodedImage).webp(webpOptions)
                 const sharpJpg = sharp(decodedImage).jpeg(jpegOptions)
+                const sharpThumb = sharp(decodedImage).resize(thumbSize, thumbSize, { fit:'inside' }).jpeg(thumbOptions)
 
-                sharpWebp.pipe(uploadWebp(s3));
-                sharpJpg.pipe(uploadJpg(s3));
 
-                function uploadWebp(s3) {
+                sharpWebp.pipe(upload(s3, 'webp'));
+                sharpJpg.pipe(upload(s3, 'jpg'));
+                sharpThumb.pipe(upload(s3, 'jpg', true));
+
+                function upload(s3, format, thumb) {
                     const pass = new stream.PassThrough();
-
-                    const params = { Bucket: bucketName, Key: `webp/${filename}.webp`, Body: pass };
+                    const key = thumb ? `thumbs/${filename}.${format}` : `${format}/${filename}.${format}`;
+                    const params = { Bucket: bucketName, Key: key, Body: pass };
                     s3.upload(params, function(err, data) {
                         if (err) {
                             callback(err, null);
                         } else {
-                            if (oneUploaded) {
+                            if (uploaded === 2) {
                                 let response = {
                                     "statusCode": 200,
                                     "body": JSON.stringify("success"),
@@ -59,7 +71,7 @@ exports.handler = (event, context, callback) => {
                                 mongoose.disconnect();
                                 callback(null, response);
                             } else {
-                                oneUploaded = true;
+                                uploaded++;
                             }
                         }
                     });
@@ -67,30 +79,7 @@ exports.handler = (event, context, callback) => {
                     return pass;
                 }
 
-                function uploadJpg(s3) {
-                    const pass = new stream.PassThrough();
-
-                    const params = { Bucket: bucketName, Key: `jpg/${filename}.jpg`, Body: pass };
-                    s3.upload(params, function(err, data) {
-                        if (err) {
-                            callback(err, null);
-                        } else {
-                            if (oneUploaded) {
-                                let response = {
-                                    "statusCode": 200,
-                                    "body": JSON.stringify("success"),
-                                    "isBase64Encoded": false
-                                };
-                                mongoose.disconnect();
-                                callback(null, response);
-                            } else {
-                                oneUploaded = true;
-                            }
-                        }
-                    });
-
-                    return pass;
-                }
+                
             })
         });
 };
