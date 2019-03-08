5292dbaa0efe60b0dc20f284aa26ec0eaa813c59
nathanielgrossman <nategrossman@mac.com>
2019-03-08 10:14:01 -0800

    fixed behavior where update views wasn't actually updating last_viewed date

--- update-views.js
+++ update-views.js
@@ -6,7 +6,7 @@ exports.handler = (event, context, callback) => {
     mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
         .then(() => {
             Image.findByIdAndUpdate(
-                event.id, { $inc: { views: 1 } },
+                event.id, { $inc: { views: 1 }, $set: { last_viewed: Date.now() } },
                 (err, image) => {
                     if (err) {
                         let response = {
